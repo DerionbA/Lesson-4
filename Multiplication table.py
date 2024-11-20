a0cdb0f8ea9c2e9b6a85c7e7cfd328db823747a5
DerionbA <alemdavis8@gmail.com>
2024-11-20 17:26:36 -0400

    all in one

---
+++ Multiplication table.py
@@ -0,0 +1,4 @@
+number = int(input("enter a number "))
+
+for i in range(1, 13):
+    print(f'{number} X {i} = {number * i}')
---
+++ Natural Number sum.py
@@ -0,0 +1,2 @@
+total_sum = sum(range(1 , 51))
+print(f"the sum fove the firt 50 number is {total_sum}")
